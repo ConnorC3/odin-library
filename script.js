ae4ffc3d4fcbb8c22b8aebe9907835126a63450b
Connor C <164671442+ConnorC3@users.noreply.github.com>
2025-08-10 15:20:56 -0400

    Create book constructor

---
+++ script.js
@@ -0,0 +1,24 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function () {
+        let hasRead = "";
+        if (read) {
+            hasRead = "read"
+        } else {
+            hasRead = "not read yet"
+        }
+        return `${title} by ${author}, ${pages} pages, ${hasRead}`
+    }
+}
+
+let theHobbit = new Book("The Hobbit", "J.R.R Tolkien", 295, true);
+console.log(theHobbit.info())
+
+function addBookToLibrary(){
+
+}
