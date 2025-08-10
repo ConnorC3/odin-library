e30f6e1dbec8fff4791faf709ee51f4c800b5a1d
Connor C <164671442+ConnorC3@users.noreply.github.com>
2025-08-10 17:53:29 -0400

    Create addBookToLibrary method

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ function Book(title, author, pages, read){
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.id = crypto.randomUUID();
     this.info = function () {
         let hasRead = "";
         if (read) {
@@ -19,6 +20,7 @@ function Book(title, author, pages, read){
 let theHobbit = new Book("The Hobbit", "J.R.R Tolkien", 295, true);
 console.log(theHobbit.info())
 
-function addBookToLibrary(){
-
+function addBookToLibrary(title, author, pages, read){
+    let newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
 }
